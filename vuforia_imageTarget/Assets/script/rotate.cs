80f74478bd8d1520362f3c24e2fdd5ea412a8d7f
DESKTOP-MB3LBN3\dashmoment <dashmoment1017@gmail.com>
2018-06-26 00:53:21 +0800

    Vuforia image target trial

---
+++ vuforia_imageTarget/Assets/script/rotate.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class rotate : MonoBehaviour {
+
+	public GameObject player;
+	private Vector3 position;
+	public Text textvalue;
+
+	// Use this for initialization
+	void Start () {
+
+		getPosition();
+		player = GameObject.FindWithTag("rotateball");
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		getPosition();
+		player.transform.Rotate (0, 360*Time.deltaTime, 0);
+	}
+
+	void getPosition(){
+
+		position = Input.mousePosition;
+        textvalue.text = position.ToString();
+
+	}
+
+}
