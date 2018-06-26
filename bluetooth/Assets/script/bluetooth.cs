64f3f2bfcf7524c76389275434e824d0ec860342
DESKTOP-MB3LBN3\dashmoment <dashmoment1017@gmail.com>
2018-06-27 01:05:28 +0800

    add Unity android toast and enable blue tooth

---
+++ bluetooth/Assets/script/bluetooth.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bluetooth : MonoBehaviour {
+
+	AndroidJavaClass UnityPlayer;
+	AndroidJavaObject currentActivity;
+	AndroidJavaObject context;
+
+	string toastString;
+
+	// Use this for initialization
+	void Start () {
+
+		if (Application.platform == RuntimePlatform.Android)
+	        {
+	            UnityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+	            currentActivity = UnityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+	            context = currentActivity.Call<AndroidJavaObject>("getApplicationContext");
+	        }
+	    
+	    //openBlueTooth();
+	    currentActivity.Call("runOnUiThread", new AndroidJavaRunnable(openBlueTooth));
+	    this.toastString = "Open BlueTooth";
+	    this.showToast();
+	    //this.openBlueTooth();
+	    currentActivity.Call("runOnUiThread", new AndroidJavaRunnable(showToast));
+	    
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void openBlueTooth(){
+		AndroidJavaClass bluetoothAdapt = new AndroidJavaClass("android.bluetooth.BluetoothAdapter");
+		AndroidJavaObject default_adapt = bluetoothAdapt.CallStatic<AndroidJavaObject>("getDefaultAdapter");
+
+		this.toastString = string.Concat("BT Status:", default_adapt.Call<bool>("isEnabled").ToString());
+	    this.showToast();
+		
+		if(default_adapt.Call<bool>("isEnabled") == false){
+			default_adapt.Call<bool>("enable");
+			this.toastString = "BT Enable";
+	   		this.showToast();
+		}
+	}
+
+	void showToast(){
+
+		AndroidJavaClass Toast = new AndroidJavaClass("android.widget.Toast");
+		AndroidJavaObject javaString = new AndroidJavaObject("java.lang.String", toastString);
+		AndroidJavaObject toast = Toast.CallStatic<AndroidJavaObject>("makeText", context, javaString, Toast.GetStatic<int>("LENGTH_SHORT"));
+		toast.Call("show");
+
+	}
+}
---
+++ toast/toast/Assets/toast.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class toast : MonoBehaviour {
+
+	AndroidJavaClass UnityPlayer;
+	AndroidJavaObject currentActivity;
+	AndroidJavaObject context;
+
+	string toastString;
+
+  
+	void Start ()
+	{
+
+	    if (Application.platform == RuntimePlatform.Android)
+	        {
+	            UnityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+	            currentActivity = UnityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+	            context = currentActivity.Call<AndroidJavaObject>("getApplicationContext");
+	        }
+
+	        this.toastString = "Hello Unity!";
+	        currentActivity.Call("runOnUiThread", new AndroidJavaRunnable(showToast));
+	    }
+	  
+	void showToast()
+	    {
+	        AndroidJavaClass Toast = new AndroidJavaClass("android.widget.Toast");
+	        AndroidJavaObject javaString = new AndroidJavaObject("java.lang.String", toastString);
+	        AndroidJavaObject toast = Toast.CallStatic<AndroidJavaObject>("makeText", context, javaString, Toast.GetStatic<int>("LENGTH_SHORT"));
+	        toast.Call("show");
+	    }
+
+}
